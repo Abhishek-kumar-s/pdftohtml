71b25ceb7107b276c56950b7cde86ee787fc9cec
abhishk2 <abhishek.kumar12@britebill.com>
2019-12-12 21:00:01 +0200

    Initial commit

---
+++ src/main/java/com/abhishek/pdftohtml/PdftohtmlApplication.java
@@ -0,0 +1,27 @@
+package com.abhishek.pdftohtml;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.fit.pdfdom.PDFDomTree;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+@SpringBootApplication
+public class PdftohtmlApplication {
+
+    public static void main(String[] args) throws IOException, ParserConfigurationException {
+        SpringApplication.run(PdftohtmlApplication.class, args);
+        PDDocument pdf = PDDocument.load(new File("C:\\Users\\abhishk2\\Downloads\\aform.pdf"));
+        Writer output = new PrintWriter("src/output/pdf.html", "utf-8");
+        new PDFDomTree().writeText(pdf, output);
+
+        output.close();
+    }
+
+}
---
+++ src/test/java/com/abhishek/pdftohtml/PdftohtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.pdftohtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdftohtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
